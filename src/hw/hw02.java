8dd9337273ded7876e6f78f5d0fae17a28ce3a80
PenGuinMike <104021074@live.asia.edu.tw>
2016-11-03 21:25:47 +0800

    finish

--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,27 @@
 package hw;
-
+import java.util.*;
 public class hw02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		float v1 = scn.nextFloat();
+		System.out.println(fun0(n,v1));
+		
+	}
+	public static double fun0(int n,float v1){
+		double x = 0 ;
+		switch(n){
+		case 1:
+			x =(double) (v1-170)*0.6+62;
+			break;
+		case 2:
+			x =(double) (v1-158)*0.5+52;
+			break;
+		}
+		
+		return x;
 	}
 
 }
